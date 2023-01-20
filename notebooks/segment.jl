cd5c61d17ea92871ccb2e640984506bdde386ebd
Dale-Black <djblack@uci.edu>
2023-01-20 11:02:02 -0800

    init

---
+++ notebooks/segment.jl
@@ -0,0 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.19.18
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0f1ab7a0-98ec-11ed-0e70-356525ed2545
+# ╠═╡ show_logs = false
+begin
+	using Pkg; Pkg.activate(".")
+	using Revise, PlutoUI, DICOM, OrthancTools
+end
+
+# ╔═╡ 36c45072-0063-4c2d-ab47-201103f62599
+TableOfContents()
+
+# ╔═╡ Cell order:
+# ╠═0f1ab7a0-98ec-11ed-0e70-356525ed2545
+# ╠═36c45072-0063-4c2d-ab47-201103f62599
